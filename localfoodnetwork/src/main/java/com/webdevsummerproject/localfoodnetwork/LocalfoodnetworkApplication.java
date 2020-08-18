838374124738eeb9c12634d2aaa26ee7a7ace8fb
Oisín R <redmondoisin@protonmail.com>
2020-08-18 13:18:31 +0100

    added maven springboot template

---
+++ localfoodnetwork/src/main/java/com/webdevsummerproject/localfoodnetwork/LocalfoodnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.webdevsummerproject.localfoodnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalfoodnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalfoodnetworkApplication.class, args);
+	}
+
+}
---
+++ localfoodnetwork/src/test/java/com/webdevsummerproject/localfoodnetwork/LocalfoodnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdevsummerproject.localfoodnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalfoodnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
